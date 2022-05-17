94e0aa785b50ed095e95cd9cd9a3c9c57958eb0a
Lucy <lucy.d.bullen@gmail.com>
2022-05-17 17:54:11 -0600

    commit tree e6d272448d93da1f5ccc7ae15b240b88fdcfe667

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+// // code your solution here
+// function superbowlWin(record){
+//     let theElement = record.find(isWin);
+//     //console.log(theElement.year);
+//     if (!!theElement === true) {
+//         return theElement.year;
+//     }
+//     else {
+//         return undefined;
+//     }
+// };
+
+// function isWin(element, index, array) {
+//     return (element.result === "W");
+// };
+
+// other solution 
+
+const superbowlWin = (record) => {
+    const result = record.find(record => record.result === 'W')
+    return !!result ? result.year : undefined;
+};
+
+
+   
+const superbowlWin2 = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  };
